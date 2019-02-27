08f62eb18d3e707a80f4a58c2ca2152be6715174
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-02-27 11:32:25 +0000

    :sparkles: Add create new user
    Co-authored-by: Kwaku Ankobia <kwaku.ankobia@gmail.com>
    Co-authored-by: Adrien Fabre  <adrien.fabre.1@gmail.com>
    Co-authored-by: Elliot  Jennings <elliot.r.jennings@outlook.com>
    Co-authored-by: Vaith Schmitz <vaithschmitz@googlemail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  def new
+  end
+  def create
+    User.create(
+      name: params["user"]["name"],
+      email: params["user"]["email"],
+      username: params["user"]["username"],
+      password: params["user"]["password"]
+    )
+    render plain: "success!"
+  end
 end
--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -2,13 +2,12 @@ require 'rails_helper'
 
 RSpec.feature "Sign up", type: :feature do
   scenario "Can visit sign up page and sign up" do
-    visit "/"
+    visit "/users/new"
+    fill_in "user[name]", with: "ZOZOthemonkey"
+    fill_in "user[username]", with: "ZOZO"
+    fill_in "user[email]", with: "ZOZO@netscape.com"
+    fill_in "user[password]", with: "zozo1"
     click_button "Sign Up"
-    fill_in "Name", with: "ZOZO_the_monkey"
-    fill_in "Username", with: "ZOZO"
-    fill_in "Email", with: "ZOZO@netscape.com"
-    fill_in "Password", with: "zozo1"
-    click_button "Submit"
     expect(page).to have_content("success!")
   end
 end
