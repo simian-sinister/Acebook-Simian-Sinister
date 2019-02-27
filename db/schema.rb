ff711db03c53f6449e9b56ff96e47f56200f771e
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-02-27 10:36:18 +0000

    :building_construction: migrated database
    Co-authored-by: Elliot  Jennings <elliot.r.jennings@outlook.com>
    Co-authored-by: Vaith Schmitz <vaithschmitz@googlemail.com>
    Co-authored-by: Kwaku Ankobia <kwaku.ankobia@gmail.com>
    Co-authored-by: Adrien Fabre  <adrien.fabre.1@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170526114520) do
+ActiveRecord::Schema.define(version: 20190227095735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.string "email"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
