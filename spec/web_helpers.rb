cd23270dbb684455944b06811bbc9a03bfa75c3b
AdrienFabre <adrien.fabre.1@gmail.com>
2019-02-28 17:24:21 +0000

    :sparkles: Add navbar buttons
    Co-authored-by: Luca Fruzza  <lfruzza@hotmail.co.uk>

---
+++ spec/web_helpers.rb
@@ -0,0 +1,10 @@
+def auto_sign_up
+  visit "/users/sign_up"
+    fill_in "user_first_name", with: "ZOZO"
+    fill_in "user_last_name", with: "themonkey"
+    fill_in "user_username", with: "ZOZO"
+    fill_in "user_email", with: "ZOZO@netscape.com"
+    fill_in "user_password", with: "zozo1111"
+    fill_in "user_password_confirmation", with: "zozo1111"
+    click_button "Sign up"
+end
