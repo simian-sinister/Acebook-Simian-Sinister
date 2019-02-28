fd873f41175a78404167024c9fe31e9b8cf429ff
AdrienFabre <adrien.fabre.1@gmail.com>
2019-02-28 15:07:42 +0000

    :sparkles: Created nav bar, added index page.
    Co-authored-by: Kwaku Ankobia <kwaku.ankobia@gmail.com>
    Co-authored-by: Adrien Fabre  <adrien.fabre.1@gmail.com>
    Co-authored-by: Luca Fruzza  <lfruzza@hotmail.co.uk>
    Co-authored-by: Elliot  Jennings <elliot.r.jennings@outlook.com>
    Co-authored-by: Vaith Schmitz <vaithschmitz@googlemail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -22,10 +22,10 @@ ActiveRecord::Schema.define(version: 20190228111643) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
     t.string "first_name", default: "", null: false
     t.string "last_name", default: "", null: false
     t.string "username", default: "", null: false
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
