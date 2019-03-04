2eca1cb47ab08d91fcd96b56b9e8abc5819649ac
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-03-04 14:49:40 +0000

    :sparkles: added sign in functionality
    Co-authored-by: Vaith Schmitz <vaithschmitz@googlemail.com>

---
+++ spec/features/user_account_features/users_can_sign_in_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+require 'web_helper'
+
+RSpec.feature 'Sign in', type: :feature do
+  scenario 'Users can sign in' do
+    sign_up_nuffmunz
+    expect(page).to have_content("Welcome, nuffmunz")
+    click_on "Sign out"
+    expect(page).to_not have_content("Welcome, nuffmunz")
+    click_on "Sign in"
+    fill_in 'user_email', with: 'rich@money.cash'
+    fill_in 'user_password', with: 'Neopets 4eva'
+    click_on 'sign in'
+    expect(page).to have_content("Welcome, nuffmunz")
+  end
+end
