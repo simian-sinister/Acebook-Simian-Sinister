d1e09956c76bf312fdd17caf8d6dc1d2d81b2e36
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-03-04 12:19:37 +0000

    :card_file_box: added user attribute migrations
    Co-authored-by: Luca Fruzza  <lfruzza@hotmail.co.uk>

--- spec/features/user_account_features/user_can_sign_up_spec.rb
+++ spec/features/user_account_features/user_can_sign_up_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.feature 'Sign', type: :feature do
   scenario 'Users can sign up' do
     visit '/users/sign_up'
-    fill_in 'first_name', with: 'Money'
-    fill_in 'last_name', with: 'McBags'
-    fill_in 'username', with: 'nuffmunz'
-    fill_in 'email', with: 'rich@money.cash'
-    fill_in 'password', with: 'Neopets 4eva'
-    fill_in 'password_confirmation', with: 'Neopets 4eva'
+    fill_in 'user_first_name', with: 'Money'
+    fill_in 'user_last_name', with: 'McBags'
+    fill_in 'user_username', with: 'nuffmunz'
+    fill_in 'user_email', with: 'rich@money.cash'
+    fill_in 'user_password', with: 'Neopets 4eva'
+    fill_in 'user_password_confirmation', with: 'Neopets 4eva'
     click_on 'Sign up'
     expect(page).to have_content("welcome, nuffmunz")
   end
