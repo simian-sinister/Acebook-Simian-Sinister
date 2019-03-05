e2df4f7685c8377cccc8b9547d29ef65fdc823ba
AdrienFabre <adrien.fabre.1@gmail.com>
2019-03-05 14:16:13 +0000

    :art: Refactor test can submit posts and view them
    Co-authored-by: Elliot  Jennings <elliot.r.jennings@outlook.com>
    Co-authored-by: Luca Fruzza  <lfruzza@hotmail.co.uk>

--- spec/features/post_features/post_crud_spec.rb
+++ spec/features/post_features/post_crud_spec.rb
@@ -5,9 +5,7 @@ RSpec.feature 'CRUD post', type: :feature do
   scenario 'Can submit posts and view them' do
     sign_up_nuffmunz
     expect(page).to have_content("Welcome, nuffmunz")
-    click_link 'New post'
-    fill_in 'Message', with: 'Hello, world!'
-    click_on 'Submit'
+    create_post_hello_world
     expect(page).to have_content('Hello, world!')
   end
 
