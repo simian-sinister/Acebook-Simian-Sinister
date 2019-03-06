cd6de7cc85cb98824ea23576a8c3606b271553e6
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-03-06 12:51:56 +0000

    :white_check_mark: add upload profile picture at sign up test
    Co-authored-by: Elliot  Jennings <elliot.r.jennings@outlook.com>

--- spec/features/user_account_features/user_can_sign_up_spec.rb
+++ spec/features/user_account_features/user_can_sign_up_spec.rb
@@ -12,4 +12,17 @@ RSpec.feature 'Sign', type: :feature do
     click_on 'Sign up'
     expect(page).to have_content("Welcome, nuffmunz")
   end
+
+  scenario 'User can attach a profile picture at signup' do
+    visit '/users/sign_up'
+    fill_in 'user_first_name', with: 'Money'
+    fill_in 'user_last_name', with: 'McBags'
+    fill_in 'user_username', with: 'nuff munz'
+    fill_in 'user_email', with: 'rich@money.cash'
+    fill_in 'user_password', with: 'Neopets 4eva'
+    fill_in 'user_password_confirmation', with: 'Neopets 4eva'
+    attach_file("Choose file", Rails.root + "spec/fixtures/monkeyboi.jpg")
+    click_on 'Sign up'
+    expect(page.find('#profile-picture')['src']).to have_content 'monkeyboi.jpg'
+  end
 end
