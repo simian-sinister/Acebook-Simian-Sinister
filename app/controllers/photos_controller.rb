84ff04e93647ecb411584a26f8da26d9795be70e
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-03-07 11:39:07 +0000

    active setup

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -69,6 +69,6 @@ class PhotosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def photo_params
-      params.require(:photo).permit(:caption, :photo)
+      params.require(:photo).permit(:caption, :photo, :user_id)
     end
 end
