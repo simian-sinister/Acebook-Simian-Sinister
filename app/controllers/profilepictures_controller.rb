97ceac028b1fb5a478f847209a9127034d7be62d
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-03-08 12:17:14 +0000

    :construction: add css to edit profile pic
    Co-authored-by: Kwaku Ankobia <kwaku.ankobia@gmail.com>

--- app/controllers/profilepictures_controller.rb
+++ app/controllers/profilepictures_controller.rb
@@ -1,4 +1,18 @@
 class ProfilepicturesController < ApplicationController
+
   def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.profile_picture.attach(params[:profile_picture])
+    redirect_to posts_url
+    end
+
+  def destroy
+    @user = current_user
+    @user.profile_picture.purge
+    redirect_to posts_url
   end
 end
