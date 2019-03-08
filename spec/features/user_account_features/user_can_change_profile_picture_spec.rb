fad4e7822206363cfbfdd803c01aff9ab5fee03c
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-03-08 14:17:15 +0000

    :art: test pass for css profile pic update
    Co-authored-by: Kwaku Ankobia <kwaku.ankobia@gmail.com>

--- spec/features/user_account_features/user_can_change_profile_picture_spec.rb
+++ spec/features/user_account_features/user_can_change_profile_picture_spec.rb
@@ -3,12 +3,12 @@ require 'web_helper'
 
 RSpec.feature 'Sign', type: :feature do
 
-  scenario 'User can attach a profile picture at signup' do
+  scenario 'User can update a profile picture at signup' do
     sign_up_with_picture_nuffmunz
     expect(page.find('#profile-picture img')['src']).to have_content 'monkeyboi.jpg'
     click_on "update profile picture"
     attach_file("Profile picture", Rails.root + "spec/fixtures/monkeyboi2.jpg")
-    click_on "update"
+    click_on "Update"
     expect(page.find('#profile-picture img')['src']).to_not have_content 'monkeyboi.jpg'
     expect(page.find('#profile-picture img')['src']).to have_content 'monkeyboi2.jpg'
   end
